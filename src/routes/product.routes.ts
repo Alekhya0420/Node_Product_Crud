5471b3c6e33c560e39833ab692a1ca724e46e068
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-03 13:11:23 +0530

    multer handeling

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,12 +6,13 @@ import {
   updateProduct,
   deleteProduct,
 } from '../controllers/product.controller';
+import { upload } from '../middlewares/multer';
 
 const router = Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', createProduct);
+router.post('/', upload.single('file'), createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
