f453e5017ab493bbd1d9e12c1c860d5adffbcccd
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-03 18:54:21 +0530

    Status Change api implementation along with searching functionality in list api

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -5,15 +5,18 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  updateProductStatus
 } from '../controllers/product.controller';
 import { upload } from '../middlewares/multer';
 
 const router = Router();
-
+//api endpoints
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 router.post('/', upload.single('file'), createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
+router.patch('/:id', updateProductStatus);
+
 
 export default router;
