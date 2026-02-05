a597a58d3934c7f9fe1c8175996cce052662f8a2
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-05 12:57:12 +0530

    forgot-password and reset-password api implementation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,8 +4,9 @@ import {
   login,
   refreshToken,
   logout,
-  forgotPassword,
+  forgotPasswordOTP,
   resetPassword,
+  verifyOtp,
 } from "../controllers/auth.controller";
 
 const router = Router();
@@ -14,7 +15,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
-router.post("/forgot-password", forgotPassword);
+router.post("/forgot-password", forgotPasswordOTP);
+router.post("/verify-otp", verifyOtp);
 router.post("/reset-password", resetPassword);
 
 export default router;
--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,10 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  // host: process.env.MAIL_HOST,
   service:"gmail",
-  // port: Number(process.env.MAIL_PORT),
-  // secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
@@ -24,7 +21,7 @@ export const sendMail = async ({
 }) => {
   await transporter.sendMail({
     from: process.env.MAIL_FROM,
-    to:'admin@yopmail.com',
+    to:'adminnewone@yopmail.com',
     subject,
     html,
   });
