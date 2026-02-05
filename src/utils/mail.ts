16917747d4b50d1d4abada963885ac72e70d899b
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-05 13:56:54 +0530

    mail.ts changes

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,14 +1,39 @@
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service:"gmail",
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
+
+// export const sendMail = async ({
+//   to,
+//   subject,
+//   html,
+// }: {
+//   to: string;
+//   subject: string;
+//   html: string;
+// }) => {
+//   await transporter.sendMail({
+//     from: process.env.MAIL_FROM,
+//     to:'adminnewone@yopmail.com',
+//     subject,
+//     html,
+//   });
+// };
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service:"gmail",
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 });
-console.log("MAIL_USER:", process.env.MAIL_USER);
-console.log("MAIL_PASS:", process.env.MAIL_PASS);
 
 export const sendMail = async ({
   to,
@@ -20,8 +45,8 @@ export const sendMail = async ({
   html: string;
 }) => {
   await transporter.sendMail({
-    from: process.env.MAIL_FROM,
-    to:'adminnewone@yopmail.com',
+    from: process.env.MAIL_FROM || process.env.MAIL_USER,
+    to,               
     subject,
     html,
   });
