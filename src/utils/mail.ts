b9436a4f90aa7e3fa3556fff761591b30a127077
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-10 12:33:15 +0530

    manage mail.ts

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,31 +1,4 @@
-import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  service:"gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-console.log("MAIL_USER:", process.env.MAIL_USER);
-console.log("MAIL_PASS:", process.env.MAIL_PASS);
-
-export const sendMail = async ({
-  to,
-  subject,
-  html,
-}: {
-  to: string;
-  subject: string;
-  html: string;
-}) => {
-  await transporter.sendMail({
-    from: process.env.MAIL_FROM,
-    to:'adminnewone@yopmail.com',
-    subject,
-    html,
-  });
-};
 
 // import nodemailer from "nodemailer";
 
